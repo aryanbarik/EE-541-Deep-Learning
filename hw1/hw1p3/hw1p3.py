1ee5a8904aa32398f3bf5e88f102619022c05631
Aryan Barik <aryan.kunmoon@gmail.com>
2023-08-30 14:43:35 -0700

    getting too many iterations but correct outputs

--- hw1/hw1p3/hw1p3.py
+++ hw1/hw1p3/hw1p3.py
@@ -2,10 +2,8 @@ from func import f
 import sys
 
 if len(sys.argv)!= 3:
-    print('Range error', file=sys.stderr)
-
-a = sys.argv[0]
-b = sys.argv[1]
+    print('range error', file=sys.stderr)
+    sys.exit()
 
 def secant(a,b):
     if not (a < b or f(a)*f(b) < 0):
@@ -18,16 +16,19 @@ def secant(a,b):
     x3 = 0
     iterations = 0
     
-    while ( abs(x3 - x2) >= 10**-10 or abs(x2 - x1) >= 10**-10 ):
+    while ( abs(x3 - x2) > 10**-10 or abs(x2 - x1) > 10**-10 ):
         x3 = x2 - (f(x2) * (x2 - x1)/(f(x2) - f(x1)))
         iterations += 1
         
         x1 = x2
         x2 = x3
         
-    print(iterations, x1, x2, x3, sep="\n", file=sys.stdout)
-        
+    print(iterations, file=sys.stdout)
+    print(x1, file=sys.stdout)
+    print(x2, file=sys.stdout)
+    print(x3, file=sys.stdout)
     
-        
+a = float(sys.argv[1])
+b = float(sys.argv[2])
 
-        
+secant(a,b)
