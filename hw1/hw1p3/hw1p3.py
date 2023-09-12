57b0f4a8449dcb7f0af0fbde64f64ac3040fc4aa
Aryan Barik <aryan.kunmoon@gmail.com>
2023-09-12 13:58:11 -0700

    started hw2

--- hw1/hw1p3/hw1p3.py
+++ hw1/hw1p3/hw1p3.py
@@ -6,19 +6,22 @@ if len(sys.argv)!= 3:
     sys.exit()
 
 def secant(a,b):
-    if not (a < b or f(a)*f(b) < 0):
+    if (a >= b or f(a)*f(b) >= 0):
         print('range error', file=sys.stderr)
         return
     
     # while not within convergence
-    x2 = b
     x1 = a
-    x3 = 0
+    x2 = b
+    x3 = a
     iterations = 0
     
-    while ( abs(x3 - x2) > 10**-10 or abs(x2 - x1) > 10**-10 ):
+    while ( (abs(x3 - x2) >= 10**-10) or (abs(x2 - x1) >= 10**-10)):
         x3 = x2 - (f(x2) * (x2 - x1)/(f(x2) - f(x1)))
         iterations += 1
+        
+        if ((abs(x3 - x2) < 10**-10) or (abs(x2 - x1) < 10**-10)):
+            break
         
         x1 = x2
         x2 = x3
@@ -27,8 +30,11 @@ def secant(a,b):
     print(x1, file=sys.stdout)
     print(x2, file=sys.stdout)
     print(x3, file=sys.stdout)
-    
-a = float(sys.argv[1])
-b = float(sys.argv[2])
+
+try:
+    a = float(sys.argv[1])
+    b = float(sys.argv[2])
+except ValueError:
+    print('range error')
 
 secant(a,b)
